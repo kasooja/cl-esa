10eddf4b8cec54edbd45276ece95586a6b988794
Kartik <asooja@gmail.com>
2013-11-17 14:04:34 +0200

    Fixed bugs in CLESATrove.java

--- ds.clesa/src/main/java/eu/monnetproject/clesa/ds/clesa/CLESATrove.java
+++ ds.clesa/src/main/java/eu/monnetproject/clesa/ds/clesa/CLESATrove.java
@@ -14,11 +14,9 @@ import eu.monnetproject.clesa.core.lang.Language;
 import eu.monnetproject.clesa.core.tokenizer.Tokenizer;
 import eu.monnetproject.clesa.core.utils.Pair;
 import eu.monnetproject.clesa.core.utils.TextNormalizer;
-import eu.monnetproject.clesa.core.utils.Vector;
-import eu.monnetproject.clesa.core.utils.VectorUtils;
 import eu.monnetproject.clesa.lucene.basic.AnalyzerFactory;
 import eu.monnetproject.clesa.lucene.basic.Searcher;
-import gnu.trove.map.hash.THashMap;
+import gnu.trove.map.hash.TIntDoubleHashMap;
 
 
 /**
@@ -60,7 +58,7 @@ public class CLESATrove {
 	private static Properties config = new Properties();
 	private String multiLingualIndexPathToRead; 
 	private int lucHits = 1000;
-	private int noOfTopicsToBeCompared = 1000;
+//	private int noOfTopicsToBeCompared = 1000;
 	private Searcher searcher;
 	private Map<Language, Tokenizer> langTokenizerMap = new HashMap<Language, Tokenizer>();
 
@@ -68,7 +66,7 @@ public class CLESATrove {
 		loadConfig("load/eu.monnetproject.clesa.CLESA.properties");
 		setConfig();
 	}
-	
+
 	public CLESATrove(String configFilePath) {
 		loadConfig(configFilePath);
 		setConfig();
@@ -79,16 +77,16 @@ public class CLESATrove {
 		CLESATrove.config = config;
 		setConfig();
 	}	
-	
+
 	private void setConfig(){
-			multiLingualIndexPathToRead = config.getProperty("multiLingualIndexPathToRead");
-			noOfTopicsToBeCompared = Integer.parseInt(config.getProperty("noOfTopicsToBeCompared"));
-			lucHits = Integer.parseInt(config.getProperty("lucHits"));
-			boolean onRAM = false;
-			searcher = new Searcher(multiLingualIndexPathToRead, onRAM);
+		multiLingualIndexPathToRead = config.getProperty("multiLingualIndexPathToRead");
+	//	noOfTopicsToBeCompared = Integer.parseInt(config.getProperty("noOfTopicsToBeCompared"));
+		lucHits = Integer.parseInt(config.getProperty("lucHits"));
+		boolean onRAM = false;
+		searcher = new Searcher(multiLingualIndexPathToRead, onRAM);
 	}	
 
-	
+
 	private void loadConfig(String configFilePath){
 		try {
 			config.load(new FileInputStream(configFilePath));
@@ -120,58 +118,62 @@ public class CLESATrove {
 		String phrase2 = normalize(pair2.getFirst(), pair2.getSecond());
 		pair1 = new Pair<String, Language>(phrase1, pair1.getSecond());
 		pair2 = new Pair<String, Language>(phrase2, pair2.getSecond());		
-		Map<String, THashMap<Integer, Double>> vectorMap = new HashMap<String, THashMap<Integer, Double>>();
+		Map<String, TIntDoubleHashMap> vectorMap = new HashMap<String, TIntDoubleHashMap>();
 		Barrier barrier = new Barrier(2);
 		new TopicVectorThread("vector1", barrier, vectorMap, pair1, searcher).start();
 		new TopicVectorThread("vector2", barrier, vectorMap, pair2, searcher).start();
 		barrier.barrierWait();		
-		THashMap<Integer, Double> vector1 = vectorMap.get("vector1");
-		THashMap<Integer, Double> vector2 = vectorMap.get("vector2");				
+		TIntDoubleHashMap vector1 = vectorMap.get("vector1");
+		TIntDoubleHashMap vector2 = vectorMap.get("vector2");				
 		//VectorUtils<String> vecUtils = new VectorUtils<String>();
-//		vector1 = vecUtils.vectorWithPrincipalDimensions(vector1, noOfTopicsToBeCompared);
-//		vector2 = vecUtils.vectorWithPrincipalDimensions(vector2, noOfTopicsToBeCompared);		
+		//		vector1 = vecUtils.vectorWithPrincipalDimensions(vector1, noOfTopicsToBeCompared);
+		//		vector2 = vecUtils.vectorWithPrincipalDimensions(vector2, noOfTopicsToBeCompared);		
 		return cosineProduct(vector1, vector2);	
 	}
 
-	
-	
-	public double cosineProduct(THashMap<Integer, Double> map1, THashMap<Integer, Double> map2){
+
+	//	i = 0
+	//			j = 0
+	//			result = 0
+	//			while i < length(vec1) && j < length(vec2):
+	//			  if vec1[i].key == vec2[j].key:
+	//			    result = result + vec1[i].value * vec2[j].value
+	//			  else if vec1[i].key < vec2[j].key:
+	//			    i = i + 1
+	//			  else
+	//			    j = j + 1
+
+
+	public double cosineProduct(TIntDoubleHashMap map1, TIntDoubleHashMap map2){
 		if(map1 == null || map2 == null)
-			return 0.0;
-		
+			return 0.0;		
 		double magnitudeVector1 = 0.0;
 		double magnitudeVector2 = 0.0;
-		double dotProduct = 0.0;
-		
-		THashMap<Integer, Double> iteratingMap = null;
-		THashMap<Integer, Double> secMap = null;
-		
+		double dotProduct = 0.0;					
+		TIntDoubleHashMap iteratingMap = null;
+		TIntDoubleHashMap secMap = null;		
 		if(map1.size() >= map2.size()){
-			iteratingMap = map2;
-			secMap = map1;
-		}
-		else {
 			iteratingMap = map1;
 			secMap = map2;
 		}
-		
-		for(int unitVector : iteratingMap.keySet()){
-
+		else {
+			iteratingMap = map2;
+			secMap = map1;
+		}		
+		int[] secKeys = secMap.keys();
+		int i = 0;		
+		for(int unitVector : iteratingMap.keys()){
 			Double score1 =  iteratingMap.get(unitVector);
 			Double score2 = 0.0;
-
-			if(secMap.containsKey(unitVector))
-				score2 = secMap.get(unitVector);			
-
-			dotProduct = dotProduct + score1*score2;
-
+			if(secMap.containsKey(unitVector)){
+				score2 = secMap.get(unitVector);
+				dotProduct = dotProduct + score1*score2;
+			}
+			if(i < secKeys.length){
+				score2 = secMap.get(secKeys[i++]);
+				magnitudeVector2 = magnitudeVector2 + score2 * score2;			
+			}		
 			magnitudeVector1 = magnitudeVector1 + score1*score1;
-	
-		}
-
-		for(int unitVector : secMap.keySet()) {
-			Double score2 = secMap.get(unitVector);
-			magnitudeVector2 = magnitudeVector2 + score2*score2;			
 		}
 
 		if((magnitudeVector1 > 0.0)&&(magnitudeVector2 > 0.0))
@@ -182,56 +184,56 @@ public class CLESATrove {
 		return dotProduct;
 	}
 
-	
-//	public double scoreAgainstVector(Pair<String, Language> pair1, Vector<String> vector2) {
-//		String phrase1 = normalize(pair1.getFirst(), pair1.getSecond());
-//		pair1 = new Pair<String, Language>(phrase1, pair1.getSecond());
-//		Map<String, Vector<String>> vectorMap = new HashMap<String, Vector<String>>();
-//		Barrier barrier = new Barrier(1);
-//		new TopicVectorThread("vector1", barrier, vectorMap, pair1, searcher).start();
-//		barrier.barrierWait();		
-//		Vector<String> vector1 = vectorMap.get("vector1");
-//		VectorUtils<String> vecUtils = new VectorUtils<String>();
-//		//vector1 = vecUtils.vectorWithPrincipalDimensions(vector1, noOfTopicsToBeCompared);
-//		return vecUtils.cosineProduct(vector1, vector2);	
-//	}
-	
-//	public Vector<String> getVector(Pair<String, Language> pair) {
-//		String phrase = normalize(pair.getFirst(), pair.getSecond());
-//		pair = new Pair<String, Language>(phrase, pair.getSecond());
-//			
-//		Map<String, Vector<String>> vectorMap = new HashMap<String, Vector<String>>();		
-//		Barrier barrier = new Barrier(1);
-//		new TopicVectorThread("vector", barrier, vectorMap, pair, searcher).start();
-//		barrier.barrierWait();		
-//		Vector<String> vector = vectorMap.get("vector");
-//		return vector;
-//	}
 
-	public THashMap<Integer, Double> getVector(Pair<String, Language> pair) {
+	//	public double scoreAgainstVector(Pair<String, Language> pair1, Vector<String> vector2) {
+	//		String phrase1 = normalize(pair1.getFirst(), pair1.getSecond());
+	//		pair1 = new Pair<String, Language>(phrase1, pair1.getSecond());
+	//		Map<String, Vector<String>> vectorMap = new HashMap<String, Vector<String>>();
+	//		Barrier barrier = new Barrier(1);
+	//		new TopicVectorThread("vector1", barrier, vectorMap, pair1, searcher).start();
+	//		barrier.barrierWait();		
+	//		Vector<String> vector1 = vectorMap.get("vector1");
+	//		VectorUtils<String> vecUtils = new VectorUtils<String>();
+	//		//vector1 = vecUtils.vectorWithPrincipalDimensions(vector1, noOfTopicsToBeCompared);
+	//		return vecUtils.cosineProduct(vector1, vector2);	
+	//	}
+
+	//	public Vector<String> getVector(Pair<String, Language> pair) {
+	//		String phrase = normalize(pair.getFirst(), pair.getSecond());
+	//		pair = new Pair<String, Language>(phrase, pair.getSecond());
+	//			
+	//		Map<String, Vector<String>> vectorMap = new HashMap<String, Vector<String>>();		
+	//		Barrier barrier = new Barrier(1);
+	//		new TopicVectorThread("vector", barrier, vectorMap, pair, searcher).start();
+	//		barrier.barrierWait();		
+	//		Vector<String> vector = vectorMap.get("vector");
+	//		return vector;
+	//	}
+
+	public TIntDoubleHashMap getVector(Pair<String, Language> pair) {
 		//THashMap<K, V>
 		String phrase = normalize(pair.getFirst(), pair.getSecond());
 		pair = new Pair<String, Language>(phrase, pair.getSecond());		
-		Map<String, THashMap<Integer, Double>> vectorMap = new HashMap<String, THashMap<Integer, Double>>();		
+		Map<String, TIntDoubleHashMap> vectorMap = new HashMap<String, TIntDoubleHashMap>();		
 		Barrier barrier = new Barrier(1);
 		new TopicVectorThread("vector", barrier, vectorMap, pair, searcher).start();
 		barrier.barrierWait();		
 		return vectorMap.get("vector");
 	}
 
-	
-	
+
+
 	private class TopicVectorThread extends Thread {
 		private Barrier barrier;
 		//public Map<String, Vector<String>> vectorMap;
-		public Map<String, THashMap<Integer, Double>> vectorMapT;
-		
-	
+		public Map<String, TIntDoubleHashMap> vectorMapT;
+
+
 		private Searcher searcher;
 		private String threadName;
 		private Pair<String, Language> pair;
 
-		public TopicVectorThread(String threadName, Barrier barrier, Map<String, THashMap<Integer, Double>> vectorMapT, Pair<String, Language> pair, Searcher searcher){
+		public TopicVectorThread(String threadName, Barrier barrier, Map<String, TIntDoubleHashMap> vectorMapT, Pair<String, Language> pair, Searcher searcher){
 			super(threadName);		
 			this.threadName = threadName;
 			this.barrier = barrier;
@@ -248,7 +250,9 @@ public class CLESATrove {
 
 		public void getVector() {
 			//Map<String, Double> uriWeightMap = new HashMap<String, Double>();
-			THashMap<Integer, Double> vecMapT = new THashMap<Integer, Double>();			
+			//THashMap<Integer, Double> vecMapT = new THashMap<Integer, Double>();
+			TIntDoubleHashMap vecMapT = new TIntDoubleHashMap();	
+
 			String fieldName = pair.getSecond().getIso639_1() + "TopicContent";
 			TopScoreDocCollector docsCollector = searcher.search(pair.getFirst(), lucHits, fieldName, AnalyzerFactory.getAnalyzer(pair.getSecond()));			
 			if(docsCollector == null) {
@@ -268,7 +272,7 @@ public class CLESATrove {
 			vectorMapT.put(threadName, vecMapT);
 		}
 	}	
-	
+
 	public static void main(String[] args) {
 		String text1 = args[0];
 		String text2 = args[1];
